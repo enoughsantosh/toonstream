7408ff642cedf09adcb90a755c20b8e988dc33cb
ENOUGH BRO <kalu4134@gmail.com>
2025-03-09 22:42:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -209,6 +209,9 @@ def scrape_anime_details(q: str = Query(..., description="Path of the series or 
     background_image = background_tag["src"] if background_tag else None
     description_tag = soup.select_one(".description p")
     description = description_tag.text.strip() if description_tag else None
+     # Extract post ID from <article id="post-153">
+    post_id = soup.find("article", class_="post")["id"].replace("post-", "")
+
 
     # Check if it's a series or a movie based on the URL pattern
     if q.startswith("/series/"):
@@ -221,6 +224,7 @@ def scrape_anime_details(q: str = Query(..., description="Path of the series or 
         return {
             "type": "series",
             "title": title,
+            "post_id": post_id,
             "thumbnail": thumbnail,
             "background_image": background_image,
             "description": description,
@@ -241,6 +245,7 @@ def scrape_anime_details(q: str = Query(..., description="Path of the series or 
         return {
             "type": "movie",
             "title": title,
+            "post_id": post_id,
             "thumbnail": thumbnail,
             "background_image": background_image,
             "description": description,
